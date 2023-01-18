b9a327a5f0fc30765d6231bb0cf13529640cbc84
MBilawalGhaffar <bilawalg100104@gmail.com>
2023-01-19 01:47:59 +0500

    19/1

---
+++ app/src/main/java/com/example/qrcodefyp/activity/SigUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.qrcodefyp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.qrcodefyp.R;
+
+public class SigUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sig_up);
+    }
+}
